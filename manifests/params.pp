efabf87fadf0aea0854ac94d43f95dd756d33c79
Martin Alfke <martin.alfke@epost-dev.de>
2013-11-18 10:38:19 +0100

    add params for os specific stuff

---
+++ manifests/params.pp
@@ -0,0 +1,14 @@
+# Class backupexec::params
+#
+# set distribution specific variables
+#
+class backupexec::params {
+    case $::osfamily {
+        'RedHat': {
+            $pkgname = 'VRTSralus'
+        }
+        default: {
+            fail ("Your OS : ${::operatingsystem} is not supported.")
+        }
+    }
+}
