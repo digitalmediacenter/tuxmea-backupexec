34d67085b60ab2c9967cca1383c732ce3247f2a5
Matthias Baur <matthias.baur@dmc.de>
2014-09-16 14:10:22 +0200

    Make init class inherit from params

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,12 +2,11 @@
 #
 # This class installs and configures a Symantec BackupExec agent on Linux
 #
-class backupexec {
-    include backupexec::params
+class backupexec (
+) inherits backupexec::params {
     include backupexec::install
     include backupexec::config
     include backupexec::service
-    Class['backupexec::params'] ->
       Class['backupexec::install'] ->
       Class['backupexec::config'] ->
       Class['backupexec::service']
